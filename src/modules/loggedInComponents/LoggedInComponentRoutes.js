c6fc39291446c9fb3827060e1190b8589d9907e5
cypher33 <rohitkumar25.rkl@gmail.com>
2022-12-25 15:48:29 +0530

    code cleanup

--- src/modules/loggedInComponents/LoggedInComponentRoutes.js
+++ src/modules/loggedInComponents/LoggedInComponentRoutes.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import NotFoundError from "../../components/NotFoundError";
 import Home from "./home/Home";
 import Navbar from "./navigation/Navbar";
 import Profile from "./profile/Profile";
+import Drawer from './Drawer';
 
 const LoggedInComponentRoutes = () => {
+    const [openDrawer, setOpenDrawer] = useState(false);
+
 	return (
 		<div>
-			<Navbar />
-			<div className="tw-p-2 md:tw-max-w-2xl lg:tw-max-w-4xl xl:tw-max-w-5xl tw-items-center tw-justify-between tw-mx-auto">
+			<Drawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
+			<Navbar openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
+			<div className={`tw-p-2 md:tw-max-w-2xl lg:tw-max-w-4xl xl:tw-max-w-5xl tw-items-center tw-justify-between tw-mx-auto`}>
 				<Switch>
 					<Route exact path="/">
 						<Home />
--- src/modules/loggedInComponents/navigation/Navbar.js
+++ src/modules/loggedInComponents/navigation/Navbar.js
@@ -7,8 +7,8 @@ import NavLeftSide from './NavLeftSide';
 import NavRightSide from './NavRightSide';
 import { CricFunnImage } from '../../../resources/icons/Icons';
 
-const Navbar = () => {
-    const [openDrawer, setOpenDrawer] = useState(false);
+const Navbar = (props) => {
+    const { setOpenDrawer } = props;
     const [user, setUser] = useRecoilState(userAtom);
 
     return (
@@ -19,32 +19,6 @@ const Navbar = () => {
                     <NavRightSide setOpenDrawer={setOpenDrawer} user={user} setUser={setUser}/>
                 </div>
             </nav>
-            <Drawer
-                title={
-                    <div className="tw-flex tw-items-center">
-                        <CricFunnImage className="tw-h-10 tw-mr-3" />
-                        <span className="tw-self-center tw-text-white tw-text-xl tw-font-semibold tw-whitespace-nowrap">Cric-Funn</span>
-                    </div>
-                }
-                placement="left"
-                width="40%"
-                onClose={() => setOpenDrawer(false)}
-                className="tw-z-[3000] tw-bg-gray-900"
-                closable={true}
-                visible={openDrawer}
-                extra={
-                    <>
-                        <button>Cancel</button>
-                    </>
-                }
-            >
-                <div>Home</div>
-                <div>Profile</div>
-                <div>Tournaments</div>
-                <div>Contact Me</div>
-                <div>Raise Issue</div>
-                <div>Logout</div>
-            </Drawer>
         </div>
     );
 }
